20fc7da7dd690b23cf7ac228d3cd67a3d1353d8e
Tony Porter <tonyporter89@gmail.com>
2019-06-24 16:32:17 +0100

    rendered location icon

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React, { Component } from "react";
 import "./App.css";
-import L from 'leaflet';
+import L from "leaflet";
 import { Map, TileLayer, Marker, Popup } from "react-leaflet";
 
-var icon = L.icon({
-
-})
+var customIcon = L.icon({
+  iconUrl: require("../src/images/marker.png"),
+  iconSize: [38, 50], // size of the icon
+  iconAnchor: [22, 94], // point of the icon which will correspond to marker's location
+  shadowAnchor: [4, 62], // the same for the shadow
+  popupAnchor: [-3, -76] // point from which the popup should open relative to the iconAnchor
+});
 
 class SimpleExample extends Component<{}, State> {
   state = {
@@ -22,7 +26,7 @@ class SimpleExample extends Component<{}, State> {
           attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
-        <Marker position={position}>
+        <Marker position={position} icon={customIcon}>
           <Popup>
             A pretty CSS3 popup. <br /> Easily customizable.
           </Popup>
